0c13d1d99ef6767d80c616af4452c9145fc5e70a
unknown <nicolas.a.castaneda@berkeley.edu>
2023-05-21 13:16:06 -0700

    configured cmake project

---
+++ cuda-pathtracer/cuda-pathtracer.cpp
@@ -0,0 +1,12 @@
+﻿// cuda-pathtracer.cpp : Defines the entry point for the application.
+//
+
+#include "cuda-pathtracer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
